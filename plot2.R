9df66089bab477b0187cf3ba1efdde7245984262
Killda Chita <killdachita@gmail.com>
2015-05-09 20:49:16 -0300

    Refactor plot2.R

--- plot2.R
+++ plot2.R
@@ -31,8 +31,9 @@ data$DateTime <- strptime(
 # Subset data between the first and second date
 sub <- subset(data[c("DateTime", "Global_active_power")],
        data$Date == "1/2/2007" | data$Date == "2/2/2007")
+# Create the png file
+png("plot2.png", width = 480, height = 480, units="px")
 # Create a plot with the subset
 plot(sub, type = "l", ylab = "Global Active Power (killowatts)", xlab = "")
 # Copy the histogram to a png file
-dev.copy(png, file="plot2.png")
 dev.off()
